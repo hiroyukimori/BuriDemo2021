74d9e11b353ce1357d877c6f7b6f41186c1d326c
Hiroyuki Mori <mori@kit-eng.com>
2021-03-24 09:49:34 +0900

    Frictionless Repeated Edits Demo追加

---
+++ Demo05.FrictionlessRepeatedEdits/Class1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo05.FrictionlessRepeatedEdits
+{
+    class Class1
+    {
+        public void Mehtod1()
+        {
+            var result = Demo.Func1(10);
+            Console.WriteLine(result);
+        }
+
+        public void Mehtod2()
+        {
+            var result = Demo.Func2(10);
+            Console.WriteLine(result);
+        }
+        public void Mehtod3()
+        {
+            var result = Demo.Func3(10);
+            Console.WriteLine(result);
+        }
+
+        public void Mehtod4()
+        {
+            var result = Demo.Func4(10);
+            Console.WriteLine(result);
+        }
+
+        public void Mehtod5()
+        {
+            var result = Demo.Func5(10);
+            Console.WriteLine(result);
+        }
+
+        public void Mehtod6()
+        {
+            var result = Demo.Func6(10);
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Demo05.FrictionlessRepeatedEdits/Demo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Demo05.FrictionlessRepeatedEdits
+{
+    public class Demo
+    {
+        public static double Func1(double value) => value * 1.0;
+        public static double Func2(double value) => value * 1.0;
+        public static double Func3(double value) => value * 1.0;
+        public static double Func4(double value) => value * 1.0;
+        public static double Func5(double value) => value * 1.0;
+        public static double Func6(double value) => value * 1.0;
+        public static double Func7(double value) => value * 1.0;
+    }
+}
